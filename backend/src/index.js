cd1d92b4801f1e5d85cb8d9a11016275140123a0
YunwooKim <sym3927@naver.com>
2020-01-28 16:53:20 +0900

    Koa-Route Setting

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,11 +8,22 @@
 *===================[ Thing-Project ]===================
 */
 const Koa = require('koa');
-const app = new Koa();
+const Router = require('koa-router')
+const bodyParser = require('koa-bodyparser');
 
-app.use(ctx => {
-    ctx.body = 'hello world';
-});
+const api = require('./api');
+
+const app = new Koa();
+const router = new Router();
+
+// 라우터 설정
+router.use('/api', api.routes());
+
+// 라우터 적용 전 bodyParser
+app.use(bodyParser());
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
     console.log("Listening to port 4000");
